95e3958e29f0c619282fcaef9b874b508310b6c8
nlobachev <nikita.lobachev@fintech-farm.com>
2025-02-17 21:57:26 +0200

    Fixed cost calculation and delivery handling

--- src/app/CalcCostDelivery.java
+++ src/app/CalcCostDelivery.java
@@ -1,4 +1,18 @@
 package app;
 
-public class CalcCostDelivery {
+// Клас розрахунку вартості товару,
+// враховуючи вартість доставки
+// ЗАВДАННЯ: Виправити код класу.
+// Клас успадковує клас CalcCostBase.
+public class CalcCostDelivery extends CalcCostBase {
+
+    // Вартість доставки
+    private final static double deliveryPrice = 3.0;
+
+    // Розрахунок вартості товару,
+    // з урахуванням вартості доставки
+    @Override
+    public double calcCost(Product product) {
+        return super.calcCost(product) + deliveryPrice;
+    }
 }
